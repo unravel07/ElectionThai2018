bd2fdf44ed5812437d7e94d589454ae3f8810bee
Kritsana <tragoolphrom@gmail.com>
2018-11-01 17:12:39 +0700

    create models

--- ElectionThai2018/src/app/models.ts
+++ ElectionThai2018/src/app/models.ts
@@ -1,13 +1,45 @@
 export class ElectionArea {
-    name : string;
-    totalScore : number;
-    standardScore : number;
-    goalScore : number;
-    expectedScore : number;
-    missingScore : number;
-    percentTotalScore : number;
+    name: string;
+    totalScore: number;
+    standardScore: number;
+    goalScore: number;
+    expectedScore: number;
+    missingScore: number;
+    percentTotalScore: number;
     percentStandardScore: number;
-    percentGoalScore :number;
-    percentExpectedScore : number;
-    percentMissingScore : number;
+    percentGoalScore: number;
+    percentExpectedScore: number;
+    percentMissingScore: number;
+}
+
+export class PoliticalParty {
+    name: string;
+    percent: number;
+    totalMember: number;
+    districtMember: number;
+    partyList: number;
+}
+
+export class District {
+    name: string;
+    totalVote: number;
+    goal: number;
+    vote: number;
+    PartyInDistricts: PartyInDistrict[];
+}
+
+export class PartyInDistrict {
+    name: string;
+    vote: number;
+}
+
+export class GlobalVarible {
+    static PoliticalParties: PoliticalParty[];
+
+    // static PheuThai: PoliticalParty;
+    // static Democrat: PoliticalParty;
+    // static Palangpracharath: PoliticalParty;
+    // static FutureForward: PoliticalParty;
+    // static Bhumjaithai: PoliticalParty;
+    // static Others: PoliticalParty;
 }
--- ElectionThai2018/src/pages/home/home.ts
+++ ElectionThai2018/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PoliticalParty, GlobalVarible } from '../../app/models';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,53 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  politicalParties: PoliticalParty[];
+  
   constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidEnter() {
+    if (GlobalVarible.PoliticalParties == null) {
+      GlobalVarible.PoliticalParties = [{
+        name: 'เพื่อไทย',
+        percent: 30,
+        totalMember: 150,
+        districtMember: 100,
+        partyList: 50,
+      }, {
+        name: 'ประชาธิปัตย์',
+        percent: 24,
+        totalMember: 120,
+        districtMember: 90,
+        partyList: 30,
+      }, {
+        name: 'พลังประชารัฐ',
+        percent: 20,
+        totalMember: 100,
+        districtMember: 70,
+        partyList: 30,
+      }, {
+        name: 'อนาคตใหม่',
+        percent: 16,
+        totalMember: 80,
+        districtMember: 60,
+        partyList: 20,
+      }, {
+        name: 'ภูมิใจไทย',
+        percent: 6,
+        totalMember: 30,
+        districtMember: 20,
+        partyList: 10,
+      }, {
+        name: 'อื่นๆ',
+        percent: 4,
+        totalMember: 20,
+        districtMember: 10,
+        partyList: 10,
+      }]
+    }
+
+    this.politicalParties = GlobalVarible.PoliticalParties;
   }
 
 }
