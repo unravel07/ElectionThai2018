8711dc1719fa461e1d8a10c18fe53912abc1a081
guntza22 <guntza22@hotmail.com>
2018-11-02 11:43:39 +0700

    update Home Chart ver.2

--- ElectionThai2018/src/pages/home/home.ts
+++ ElectionThai2018/src/pages/home/home.ts
@@ -18,6 +18,8 @@ export class HomePage {
     responsive: true
   };
   public barChartData: any[];
+  public barChartDataDistrict: any[];
+  public barChartDataPartyList: any[];
 
   constructor(public navCtrl: NavController) {
 
@@ -63,9 +65,19 @@ export class HomePage {
     this.politicalParties = GlobalVarible.PoliticalParties;
     this.barChartLabels = [this.politicalParties[0].name, this.politicalParties[1].name, this.politicalParties[2].name, this.politicalParties[3].name, this.politicalParties[4].name, this.politicalParties[5].name];
     this.barChartData = [
-      { data: [this.politicalParties[0].totalMember, this.politicalParties[1].totalMember, this.politicalParties[2].totalMember, this.politicalParties[3].totalMember, this.politicalParties[4].totalMember, this.politicalParties[5].totalMember], label: 'จำนวนสมาชิกพรรคทั้งหมด' },
+      { data: [this.politicalParties[0].totalMember, this.politicalParties[1].totalMember, this.politicalParties[2].totalMember, this.politicalParties[3].totalMember, this.politicalParties[4].totalMember, this.politicalParties[5].totalMember], label: 'จำนวนสมาชิกวุฒิสภาทั้งหมด' }
 
     ];
+
+    this.barChartDataDistrict = [
+      { data: [this.politicalParties[0].districtMember, this.politicalParties[1].districtMember, this.politicalParties[2].districtMember, this.politicalParties[3].districtMember, this.politicalParties[4].districtMember, this.politicalParties[5].districtMember], label: 'จำนวนสมาชิกวุฒิสภาแบบแบ่งเขต' }
+    ]
+
+    this.barChartDataPartyList = [
+      { data: [this.politicalParties[0].partyList, this.politicalParties[1].partyList, this.politicalParties[2].partyList, this.politicalParties[3].partyList, this.politicalParties[4].partyList, this.politicalParties[5].partyList], label: 'จำนวนสมาชิกวุฒิสภาแบบสัดส่วน' }
+    ]
+
+
   }
 
 
