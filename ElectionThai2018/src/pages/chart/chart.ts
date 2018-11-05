b3060a5597a45c44127fccecd11e54ec97c8ed19
unravel07 <mick_ben10@hotmail.com>
2018-11-05 16:49:26 +0700

    update color

--- ElectionThai2018/src/pages/chart/chart.ts
+++ ElectionThai2018/src/pages/chart/chart.ts
@@ -17,17 +17,6 @@ import { GlobalVarible, PoliticalParty } from '../../app/models';
 export class ChartPage {
 
   politicalParties: PoliticalParty[];
-  public barChartLabels: string[];
-  // public barChartLabels: string[] = ['เพื่อไทย', 'ประชาธิปัตย์', 'พลังประชารัฐ', 'อนาคตใหม่', 'ภูมิใจไทย', 'อื่นๆ'];
-  public barChartType: string = 'bar';
-  public barChartLegend: boolean = true;
-  public barChartOptions: any = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public barChartData: any[];
-  public barChartDataDistrict: any[];
-  public barChartDataPartyList: any[];
 
   constructor(public navCtrl: NavController) {
 
@@ -53,39 +42,24 @@ export class ChartPage {
       }, {
         name: 'อนาคตใหม่',
         percent: 16,
-        totalMember: 80,
+        totalMember: 78,
         districtMember: 60,
-        partyList: 20,
+        partyList: 18,
       }, {
         name: 'ภูมิใจไทย',
         percent: 6,
         totalMember: 30,
-        districtMember: 20,
-        partyList: 10,
+        districtMember: 19,
+        partyList: 11,
       }, {
         name: 'อื่นๆ',
         percent: 4,
-        totalMember: 20,
-        districtMember: 10,
-        partyList: 10,
+        totalMember: 22,
+        districtMember: 11,
+        partyList: 11,
       }]
     }
     this.politicalParties = GlobalVarible.PoliticalParties;
-    this.barChartLabels = [this.politicalParties[0].name, this.politicalParties[1].name, this.politicalParties[2].name, this.politicalParties[3].name, this.politicalParties[4].name, this.politicalParties[5].name];
-    this.barChartData = [
-      { data: [this.politicalParties[0].totalMember, this.politicalParties[1].totalMember, this.politicalParties[2].totalMember, this.politicalParties[3].totalMember, this.politicalParties[4].totalMember, this.politicalParties[5].totalMember], label: 'จำนวนสมาชิกวุฒิสภาทั้งหมด' }
-
-    ];
-
-    this.barChartDataDistrict = [
-      { data: [this.politicalParties[0].districtMember, this.politicalParties[1].districtMember, this.politicalParties[2].districtMember, this.politicalParties[3].districtMember, this.politicalParties[4].districtMember, this.politicalParties[5].districtMember], label: 'จำนวนสมาชิกวุฒิสภาแบบแบ่งเขต' }
-    ]
-
-    this.barChartDataPartyList = [
-      { data: [this.politicalParties[0].partyList, this.politicalParties[1].partyList, this.politicalParties[2].partyList, this.politicalParties[3].partyList, this.politicalParties[4].partyList, this.politicalParties[5].partyList], label: 'จำนวนสมาชิกวุฒิสภาแบบสัดส่วน' }
-    ]
-
-
   }
 
 
--- ElectionThai2018/src/pages/home/home.ts
+++ ElectionThai2018/src/pages/home/home.ts
@@ -25,45 +25,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
     this.chartE = "totlaMembers";
-    if (GlobalVarible.PoliticalParties == null) {
-      GlobalVarible.PoliticalParties = [{
-        name: 'เพื่อไทย',
-        percent: 30,
-        totalMember: 150,
-        districtMember: 100,
-        partyList: 50,
-      }, {
-        name: 'ประชาธิปัตย์',
-        percent: 24,
-        totalMember: 120,
-        districtMember: 90,
-        partyList: 30,
-      }, {
-        name: 'พลังประชารัฐ',
-        percent: 20,
-        totalMember: 100,
-        districtMember: 70,
-        partyList: 30,
-      }, {
-        name: 'อนาคตใหม่',
-        percent: 16,
-        totalMember: 80,
-        districtMember: 60,
-        partyList: 20,
-      }, {
-        name: 'ภูมิใจไทย',
-        percent: 6,
-        totalMember: 30,
-        districtMember: 20,
-        partyList: 10,
-      }, {
-        name: 'อื่นๆ',
-        percent: 4,
-        totalMember: 20,
-        districtMember: 10,
-        partyList: 10,
-      }]
-    }
+   
     this.politicalParties = GlobalVarible.PoliticalParties;
     this.barChartLabels = [this.politicalParties[0].name, this.politicalParties[1].name, this.politicalParties[2].name, this.politicalParties[3].name, this.politicalParties[4].name, this.politicalParties[5].name];
     this.barChartData = [
